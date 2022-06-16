a57f1c8860ca93a1b61acfba4856ac250e0f16dd
alejocorti <acortigiganti@udesa.edu.ar>
2022-06-16 13:18:57 -0300

    genero terminado

--- Proyecto Integrador DH/js/generos.js
+++ Proyecto Integrador DH/js/generos.js
@@ -11,14 +11,17 @@ fetch('  https://api.allorigins.win/raw?url=https://api.deezer.com/genre')
     geneross=data.data[i];
     console.log(geneross);
     doc.innerHTML+=
-     `<p>${geneross.name}<p/>
-     <img src=${geneross.picture} alt='' />`;
+     `<a href="./detail-genres.html?id=${geneross.id}"><p>${geneross.name}<p/>
+     <img src=${geneross.picture} alt='' /></a>`;
   }
  let name = document.querySelector(".opa");
  name.style.color= "white";
  name.style.fontSize= "20px";
  name.style.textAlign= "center";
  name.style.backgroundColor="black";
+
+let picture = document.querySelector(".opa");
+
 
  document.querySelector("h2").innerText="GENEROS";
 
